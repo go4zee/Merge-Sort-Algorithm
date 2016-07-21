ca8daf9abfc646b2dee7e4ca83d505359ad3bf3f
go4zee <go4zeee@gmail.com>
2016-07-21 12:30:24 -0400

    Final

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -10,6 +10,126 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            Console.WriteLine("Enter the array elements to sort(Press Esc to start sort):");
+
+            var inputNumbers = readlineWithCancel();
+            foreach (int i in inputNumbers)
+                Console.WriteLine();
+            if (NotEmpty(inputNumbers.ToList()))
+            {
+                var sorted = MergeSort(inputNumbers);
+                Console.WriteLine("Sorted Array:");
+                for (int i = 0; i < sorted.Length; i++)
+                {
+                    Console.WriteLine(sorted[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+        private static int[] MergeSort(int[] numbers)
+        {
+            if (numbers.Length <= 1) return numbers;
+
+            var left = new List<int>();
+            var right = new List<int>();
+
+            Divide(numbers, left, right);
+
+            left = MergeSort(left.ToArray()).ToList();
+            right = MergeSort(right.ToArray()).ToList();
+
+            return Merge(left, right);
+        }
+
+        private static int[] Merge(List<int> left, List<int> right)
+        {
+            var result = new List<int>();
+
+            while ((NotEmpty(left)) && (NotEmpty(right)))
+            {
+                if (left.First() <= right.First())
+                {
+                    MoveValueFromSourceToResult(left, result);
+                }
+                else
+                {
+                    MoveValueFromSourceToResult(right, result);
+                }
+            }
+
+            while ((NotEmpty(left)))
+            {
+                MoveValueFromSourceToResult(left, result);
+            }
+            while (NotEmpty(right))
+            {
+                MoveValueFromSourceToResult(right, result);
+            }
+
+            return result.ToArray();
+        }
+
+        private static void MoveValueFromSourceToResult(List<int> list, List<int> result)
+        {
+            result.Add(list.First());
+            list.RemoveAt(0);
+        }
+
+        private static bool NotEmpty(List<int> listToCheck)
+        {
+            return listToCheck.Count > 0;
+        }
+
+        private static void Divide(int[] numbers, List<int> left, List<int> right)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 > 0)
+                    left.Add(numbers[i]);
+                else
+                    right.Add(numbers[i]);
+
+            }
+        }
+
+        /// <summary>
+        /// Reads the given input stream for numbers and adds to the array until Esc key is pressed
+        /// </summary>
+        /// <returns></returns>
+        private static int[] readlineWithCancel()
+        {
+            List<int> input = new List<int>();
+            ConsoleKeyInfo cki;
+            int i = 0;
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Escape)
+                {
+                    Console.Write(key.KeyChar);
+                    var enteredKey = key.KeyChar + Console.ReadLine();
+                    if (int.TryParse(enteredKey, out i))
+                        input.Add(int.Parse(enteredKey));
+                    else
+                        Console.WriteLine("\nError: Please Enter Numbers:");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("\nEntered Array:");
+            foreach (int k in input)
+                Console.WriteLine(k);
+
+            return input.ToArray();
         }
     }
 }
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+Merge Sort Algorithm - Created By Syed Z Abbas
+
+Implements Merge Sort Algorithm Using lists
+
+Contains a input stream reading method which scans for numbers alone and starts the sort when Esc key is pressed.
+
